66650e50dc25ba2c9e00519ece00db89ff391cec
PGRSPORTSANALYTICS <roberthuss@live.se>
2026-02-19 13:33:37 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from db import get_db
+from ..db import get_db
 from app.config import get_settings
 from app.database import init_db
 from app.routers import health_router, discord_router, stripe_router
